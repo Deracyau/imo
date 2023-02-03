528061acb56581f4659d158b9d22fdc2d3ecc19e
Deracyau <123265253+Deracyau@users.noreply.github.com>
2023-02-03 21:56:28 +0700

    Create bot.php

---
+++ Gwa/bot.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(0);
+//system('clear');
+a:
+date_default_timezone_set('Asia/Jakarta');
+$timestamp = time();
+$wak = date("[H:i:s]", $timestamp);
+
+$proxy = 'http://pujcvpjv:wzjxit7uyig0@144.168.217.88:8780';
+$cok = "gc2023=1c31a4b6b5553eeaa53e8c142116487f5e4c9681; _gid=GA1.2.791762934.1675422083; __cf_bm=NHZysmdtAhZmvah_g84VMM5nx8C0f35025i3kRr1P7k-1675422099-0-AcR59BAh/NP1bQp6S8joAhuTmA4fwS9cfqzHwbegfOws8MlJxgYUr8r8pEshLdtvo8rbiFeOfIyyQVxoDy4TzO2a0M1wC5IK3LuQCsLox9dwDr7Vq2tsoDxDEOrG1pKGMxVLJatNFFhLadS6ZftN9XM=; csrf_cookie_name=f7c5f7445eed5e6d86ade9dbdbc87e40; _gat_gtag_UA_186136571_1=1; _ga_VNH8H2P83Z=GS1.1.1675422082.1.1.1675422171.38.0.0; _ga=GA1.2.1364264705.1675422083";
+$us = " Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
+
+$ua=array(
+"Host: gwaher.com", 
+"Referer: https://gwaher.com/",
+"cookie: ".$cok."",
+"user-agent: ".$us."",
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://gwaher.com/auto');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+$rex = curl_exec($ch);
+$ball = explode('<' ,explode('<button type="button" class="button-33 btn-sm">&#128176;', $rex)[1])[0];
+$tok = explode('">' ,explode('<input type="hidden" name="token" value="', $rex)[1])[0];
+$tim = explode(',' ,explode('let timer =', $rex)[1])[0];
+$gg = $tim;
+if($ball == ""){goto a;}
+echo "\033[0;37m$wak \033[0;90mBalance\033[0;92m$ball \n";
+
+for ($x=$gg;$x>0;$x--){
+echo "\r   \r";
+echo "\r  \033[0;37mwait \033[0;37m[\033[0;33m".$x."\033[0;37m] \r";
+sleep(1);
+}
+
+
+$log = "https://gwaher.com/auto/verify";
+$data = "token=".$tok."";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $log);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+$result = curl_exec($ch);
+goto a;
