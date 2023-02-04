d1518a82742f04561670fe4076f303a7b77c8d18
Deracyau <123265253+Deracyau@users.noreply.github.com>
2023-02-04 14:50:59 +0700

    Update bot.php

--- Gwa/bot.php
+++ Gwa/bot.php
@@ -7,7 +7,7 @@ $timestamp = time();
 $wak = date("[H:i:s]", $timestamp);
 
 $proxy = 'http://pujcvpjv:wzjxit7uyig0@144.168.217.88:8780';
-$cok = "gc2023=1c31a4b6b5553eeaa53e8c142116487f5e4c9681; _gid=GA1.2.791762934.1675422083; __cf_bm=bPbZs3JyOEjOJB6oymOUICzC4oqZidHPby3B2xuqvDU-1675477524-0-AYT/9ZMu6GnDL1IdJ5kKo375T+QAOiSDQItPpxgo1Jcx5LfhGs8mrPUaHIgosV9o5vVSpqzMxQ1H2P28INl3Rm6t1DmXzbMXOGihCHYgUovqPfHURs5FZxuvKD6SgoBVoeWIcObrX1JuLuEhNgSJVxk=; csrf_cookie_name=71c92fc96f700f861b78126a1df247eb; _ga_HF8YJ4L2V3=GS1.1.1675477521.2.1.1675477698.0.0.0; cf_clearance=cj5tDqlt01eUj5JqiCNoFwSiBrDcuFvkm79EyVLBBuM-1675477707-0-150; _gat_gtag_UA_186136571_1=1; _ga=GA1.1.1364264705.1675422083; _ga_VNH8H2P83Z=GS1.1.1675477522.3.1.1675477733.37.0.0";
+$cok = "gc2023=1c31a4b6b5553eeaa53e8c142116487f5e4c9681; _gid=GA1.2.791762934.1675422083; _ga_HF8YJ4L2V3=GS1.1.1675477521.2.1.1675477698.0.0.0; cf_clearance=h5rLbzAgjZShGGFY.D.EZL7G_FSp9sqfVhzxWpIj8Ag-1675494473-0-150; csrf_cookie_name=cc3ab1c7c25ae0c05cb76f2b2c13ef65; __cf_bm=B3tB3tZtA0p8mYRGeDlPGbZqBN5ilgQ7HkpRfqd.N3Q-1675496895-0-AXD+UxfwWUMXTzPhT6uasszRoUtUH9ks0bM/0qHoL2KbyVSH4SvNiFSHcxa1wimXavBa1WixEBrhV2FOmPwWnTctO0ga/Bxx6cYPZyJpFcB0kgXQujDPjNWpNphkMw0214DizdJNJuAKDL/UZ+Gqcz4=; _gat_gtag_UA_186136571_1=1; _ga=GA1.1.1364264705.1675422083; _ga_VNH8H2P83Z=GS1.1.1675496901.5.1.1675496956.5.0.0";
 $us = " Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
 
 $ua=array(
@@ -30,8 +30,8 @@ $ball = explode('<' ,explode('<button type="button" class="button-33 btn-sm">&#1
 $tok = explode('">' ,explode('<input type="hidden" name="token" value="', $rex)[1])[0];
 $tim = explode(',' ,explode('let timer =', $rex)[1])[0];
 $gg = $tim;
-if($ball == ""){goto a;}
-echo "\033[0;37m$wak \033[0;90mBalance\033[0;92m$ball \n";
+if($ball == ""){exit;}
+echo "\033[0;37m$wak \033[0;32mBalance\033[0;92m$ball \n";
 
 for ($x=$gg;$x>0;$x--){
 echo "\r   \r";
