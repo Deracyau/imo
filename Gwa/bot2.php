5747136205168ce7f85f22b236cd493acd9bce72
Deracyau <123265253+Deracyau@users.noreply.github.com>
2023-02-04 14:51:43 +0700

    Update bot2.php

--- Gwa/bot2.php
+++ Gwa/bot2.php
@@ -1,94 +1,57 @@
 <?php
 error_reporting(0);
 //system('clear');
-
-function timer($t){
-     $timr=time()+$t;
-      while(true):
-      echo "\r                                                    \r";
-      $res=$timr-time();
-      if($res < 1){break;}
-if($res==$res){
-//  $str= str_repeat("\033[1;92m◼",$res)."              \r";
-}
-      echo " Please Wait ".date('i:s',$res)." ";
-      sleep(1);
-      endwhile;
-}
-
-
-function http_get($url){
-
-  $header = array(
-	 "Host: gwaher.com", 
-     "Referer: https://gwaher.com/",
-	 "cookie: _gid=GA1.2.1036907073.1675414554;_ga_HF8YJ4L2V3=GS1.1.1675420541.2.0.1675420548.0.0.0;gc2023=e86c645bb01862a7b5ade1d27299138d7a2516a9;_gat_gtag_UA_186136571_1=1;csrf_cookie_name=1fae193ab339d7580f44b5f05fc42f69;_ga=GA1.2.1632257109.1675414554;_ga_VNH8H2P83Z=GS1.1.1675420541.2.1.1675420865.27.0.0;prefetchAd_4264720=true;prefetchAd_4254767=true",
-	 "user-agent: Mozilla/5.0 (Linux; Android 10; BRAVE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36",
-);
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  return curl_exec($ch);
-}
-
-
-
-// curl post
-
-function http_post($url,$data){
-
-  $header = array(
-	 "Host: gwaher.com", 
-     
-     "Referer: https://gwaher.com/",
-	 "cookie: _gid=GA1.2.1036907073.1675414554;_ga_HF8YJ4L2V3=GS1.1.1675420541.2.0.1675420548.0.0.0;gc2023=e86c645bb01862a7b5ade1d27299138d7a2516a9;_gat_gtag_UA_186136571_1=1;csrf_cookie_name=1fae193ab339d7580f44b5f05fc42f69;_ga=GA1.2.1632257109.1675414554;_ga_VNH8H2P83Z=GS1.1.1675420541.2.1.1675420865.27.0.0;prefetchAd_4264720=true;prefetchAd_4254767=true",
-	 "user-agent: Mozilla/5.0 (Linux; Android 10; BRAVE) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Mobile Safari/537.36",
-);
-  $ch = curl_init();
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
-  curl_setopt($ch, CURLOPT_POST, 1);
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-  return curl_exec($ch);
-}
-
 a:
-$rex = "https://gwaher.com/auto";
-$rex = http_get($rex);
-$ball = explode('<' ,explode('<button type="button" class="button-33 btn-sm">&#128176;', $rex)[1])[0];
- 
-$tok = explode('">' ,explode('<input type="hidden" name="token" value="', $rex)[1])[0];
+date_default_timezone_set('Asia/Jakarta');
+$timestamp = time();
+$wak = date("[H:i:s]", $timestamp);
 
+$proxy = 'http://hdnbjrjb:yi3ukcv8u68m@188.74.210.207:6286';
+$cok = "gc2023=7c89a3bf4d32bb1de21caab1022f4005c5761e76; _gid=GA1.2.795122180.1675496605; __cf_bm=SPhQcbJVdZbE90ipSE66l0vmHI3W.eN2lSFm6FH4VEQ-1675496606-0-AWvbMjoE27Q9lJOSDk/AN4c2mkHD6i/7zDthdkuYOT0igt5B+1JG8SdPVLT5AlCTDT69oG45vskp6D7MFrW7XF+YDUlMldW4xa29CHMY56DUcWh5839ytPFOg18OJkx/igVR7wZ4VlsdIil7TAYKf7Y=; csrf_cookie_name=bea405717bfc368f40ce7c22f6293f45; cf_clearance=X.qTKmS9QJIXoVZCiAnVvTNr2au2ZbmllrwgeSgk12A-1675496660-0-150; _ga_VNH8H2P83Z=GS1.1.1675496604.1.1.1675496772.59.0.0; _ga=GA1.2.1157438538.1675496604; _gat_gtag_UA_186136571_1=1";
+$us = " Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
+
+$ua=array(
+"Host: gwaher.com", 
+"Referer: https://gwaher.com/",
+"cookie: ".$cok."",
+"user-agent: ".$us."",
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://gwaher.com/auto');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+$rex = curl_exec($ch);
+$ball = explode('<' ,explode('<button type="button" class="button-33 btn-sm">&#128176;', $rex)[1])[0];
+$tok = explode('">' ,explode('<input type="hidden" name="token" value="', $rex)[1])[0];
 $tim = explode(',' ,explode('let timer =', $rex)[1])[0];
 $gg = $tim;
-
-
+if($ball == ""){exit;}
+echo "\033[0;37m$wak \033[0;32mBalance\033[0;92m$ball \n";
 
 for ($x=$gg;$x>0;$x--){
 echo "\r   \r";
-echo "\r  Wait ".$x." \r";
+echo "\r  \033[0;37mwait \033[0;37m[\033[0;33m".$x."\033[0;37m] \r";
 sleep(1);
 }
 
 
+$log = "https://gwaher.com/auto/verify";
+$data = "token=".$tok."";
+$ch = curl_init();
 
-echo "[>] Balance =$ball \n";
-
-
-
-
-$logiin = "https://gwaher.com/auto/verify";
-$dataa = "token=".$tok."";
-$rexi = http_post($logiin,$dataa);
-
-
+curl_setopt($ch, CURLOPT_URL, $log);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+$result = curl_exec($ch);
 goto a;
-
-
-
