b506942d9e33f09865decf29dec9b7ba844c6f5e
Deracyau <123265253+Deracyau@users.noreply.github.com>
2023-02-04 09:33:32 +0700

    Update bot.php

--- Gwa/bot.php
+++ Gwa/bot.php
@@ -7,7 +7,7 @@ $timestamp = time();
 $wak = date("[H:i:s]", $timestamp);
 
 $proxy = 'http://pujcvpjv:wzjxit7uyig0@144.168.217.88:8780';
-$cok = "gc2023=1c31a4b6b5553eeaa53e8c142116487f5e4c9681; _gid=GA1.2.791762934.1675422083; __cf_bm=NHZysmdtAhZmvah_g84VMM5nx8C0f35025i3kRr1P7k-1675422099-0-AcR59BAh/NP1bQp6S8joAhuTmA4fwS9cfqzHwbegfOws8MlJxgYUr8r8pEshLdtvo8rbiFeOfIyyQVxoDy4TzO2a0M1wC5IK3LuQCsLox9dwDr7Vq2tsoDxDEOrG1pKGMxVLJatNFFhLadS6ZftN9XM=; csrf_cookie_name=f7c5f7445eed5e6d86ade9dbdbc87e40; _gat_gtag_UA_186136571_1=1; _ga_VNH8H2P83Z=GS1.1.1675422082.1.1.1675422171.38.0.0; _ga=GA1.2.1364264705.1675422083";
+$cok = "gc2023=1c31a4b6b5553eeaa53e8c142116487f5e4c9681; _gid=GA1.2.791762934.1675422083; __cf_bm=bPbZs3JyOEjOJB6oymOUICzC4oqZidHPby3B2xuqvDU-1675477524-0-AYT/9ZMu6GnDL1IdJ5kKo375T+QAOiSDQItPpxgo1Jcx5LfhGs8mrPUaHIgosV9o5vVSpqzMxQ1H2P28INl3Rm6t1DmXzbMXOGihCHYgUovqPfHURs5FZxuvKD6SgoBVoeWIcObrX1JuLuEhNgSJVxk=; csrf_cookie_name=71c92fc96f700f861b78126a1df247eb; _ga_HF8YJ4L2V3=GS1.1.1675477521.2.1.1675477698.0.0.0; cf_clearance=cj5tDqlt01eUj5JqiCNoFwSiBrDcuFvkm79EyVLBBuM-1675477707-0-150; _gat_gtag_UA_186136571_1=1; _ga=GA1.1.1364264705.1675422083; _ga_VNH8H2P83Z=GS1.1.1675477522.3.1.1675477733.37.0.0";
 $us = " Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
 
 $ua=array(
