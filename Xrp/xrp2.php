b31f997b5857328d4cedc24e785641afa171f11d
Deracyau <123265253+Deracyau@users.noreply.github.com>
2023-02-07 17:23:35 +0700

    Create xrp2.php

---
+++ Xrp/xrp2.php
@@ -0,0 +1,58 @@
+<?php
+error_reporting(0);
+system('clear');
+
+a:
+$proxy = 'http://hdnbjrjb:yi3ukcv8u68m@188.74.210.207:6286';
+$us = "Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
+$cok = "ref_id=samstonkie; PHPSESSID=57ed68f4d1b5d0b842a2e9b93744459a; Cookie_id=ed4trfgd52s4u6yex9107; _ga=GA1.1.632232455.1675761699; _ga_1H915N6YP6=GS1.1.1675761698.1.1.1675761773.0.0.0";
+$ua=array(
+"Host: www.biowallet.store", 
+"content-type: application/x-www-form-urlencoded", 
+"referer: https://www.biowallet.store/withdraw_xrp?sc=Dl45770218AQ833259958OI6646", 
+"x-requested-with: XMLHttpRequest",
+"cookie: ".$cok."",
+"user-agent: ".$us."",
+);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.biowallet.store/dashboard');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+$rex = curl_exec($ch);
+$ball = explode("'" ,explode("<a href='withdraw_xrp?sc=", $rex)[1])[0];
+//echo($ball);
+b:
+$log = "https://www.biowallet.store/includes/withdraw_xrp_func.php";
+$data = "vericode=$ball&memotag=5162606&dogeaddress=rhi77L73jGvGN3zQf3AEbYnjWYZu7CSTe8&theamount=0.0005";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $log);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+$res = curl_exec($ch);
+//echo($res);
+$suc = explode(", please wait while we reload this page" ,explode("<div class='container-fluid p-2 text-white text-center' style='background-color: green;'>", $res)[1])[0];
+if($suc == "Your withdrawal request is created"){
+echo "[>] $suc \n";
+
+}else{
+goto b;
+}
+
+
+for ($x=60;$x>0;$x--){
+echo "\r   \r";
+echo "\r  \033[0;37mwait \033[0;37m[\033[0;33m".$x."\033[0;37m] \r";
+sleep(1);
+}
+goto a;
