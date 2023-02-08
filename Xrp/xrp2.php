61a0f63f6b62dd51e3917622a62319927c287ed1
Deracyau <123265253+Deracyau@users.noreply.github.com>
2023-02-08 12:46:22 +0700

    Update xrp2.php

--- Xrp/xrp2.php
+++ Xrp/xrp2.php
@@ -2,7 +2,7 @@
 error_reporting(0);
 system('clear');
 
-a:
+
 $proxy = 'http://hdnbjrjb:yi3ukcv8u68m@188.74.210.207:6286';
 $us = "Mozilla/5.0 (Linux; Android 11; V2043) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36";
 $cok = "Cookie_id=ed4trfgd52s4u6yex9107; _ga=GA1.1.632232455.1675761699; PHPSESSID=3dbf5804d9fe68da800d99ffcc0d557c; _ga_1H915N6YP6=GS1.1.1675833962.4.1.1675833980.0.0.0";
@@ -14,20 +14,8 @@ $ua=array(
 "cookie: ".$cok."",
 "user-agent: ".$us."",
 );
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://www.biowallet.store/dashboard');
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-$rex = curl_exec($ch);
-$ball = explode("'" ,explode("<a href='withdraw_xrp?sc=", $rex)[1])[0];
-//echo($ball);
-b:
-$log = "https://www.biowallet.store/includes/withdraw_xrp_func.php";
-$data = "vericode=$ball&memotag=5162606&dogeaddress=rhi77L73jGvGN3zQf3AEbYnjWYZu7CSTe8&theamount=0.0005";
+$log = "https://www.biowallet.store/includes/login_inc.php";
+$data = "username=Tonjotenan&password=123456";
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL, $log);
@@ -39,10 +27,49 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
 curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt"); 
+$res = curl_exec($ch);
+$logg = explode("<" ,explode("<div class='alert alert-success'>", $res)[1])[0];
+echo" \033[1;90m$logg \n";
+
+a:
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://www.biowallet.store/dashboard');
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt"); 
+$rex = curl_exec($ch);
+$ball = explode("'" ,explode("<a href='withdraw_xrp?sc=", $rex)[1])[0];
+//echo($ball);
+b:
+$log = "https://www.biowallet.store/includes/withdraw_xrp_func.php";
+$data = "vericode=$ball&memotag=5162606&dogeaddress=rhi77L73jGvGN3zQf3AEbYnjWYZu7CSTe8&theamount=0.000027";
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, $log);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $ua);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_COOKIEFILE,"cookie.txt");
+curl_setopt($ch, CURLOPT_COOKIEJAR,"cookie.txt"); 
 $res = curl_exec($ch);
 //echo($res);
 $suc = explode(", please wait while we reload this page" ,explode("<div class='container-fluid p-2 text-white text-center' style='background-color: green;'>", $res)[1])[0];
 if($suc == "Your withdrawal request is created"){
+date_default_timezone_set('Asia/Jakarta');
+$timestamp = time();
+$wak = date("[H:i]", $timestamp);
 echo "\033[1;37m$wak \033[1;32m$suc \n";
 
 }else{
